68fdd78702d5af7c7e3c7fab2176ae2641943c48
bratzi <w.helwich@gmail.com>
2017-12-11 23:39:36 +0100

    Merged Scripts and Horizontal Slides
    - merged the inline-script (random-function for section 4 cards) from index.html to main.js
    - added horiz. slides to section 4 (UX/UI)

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         parallaxKey: 'd2FsZGVtYXItaGVsd2ljaC5kZV9DOTBjR0Z5WVd4c1lYZz16T24=',
         parallax: true,
+        lazyLoading: true,
         parallaxOptions: {
             type: 'reveal',
             percentage: 68
@@ -115,4 +116,44 @@ $(document).ready(function () {
         .tiType(' <strong>;-)</strong>');
 
     particlesJS.load('particles-js', 'assets/particlesjs.json', function () {});
+    
+    (function() {
+            function getRandomInt(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+            new IsoGrid(document.querySelector('.isolayer--deco1'), {
+                transform: 'translateX(33vw) translateY(-340px) rotateX(45deg) rotateZ(45deg)',
+                stackItemsAnimation: {
+                    properties: function(pos) {
+                        return {
+                            translateZ: (pos + 1) * 30,
+                            rotateZ: getRandomInt(-4, 4)
+                        };
+                    },
+                    options: function(pos, itemstotal) {
+                        return {
+                            type: dynamics.bezier,
+                            duration: 500,
+                            points: [{
+                                "x": 0,
+                                "y": 0,
+                                "cp": [{
+                                    "x": 0.2,
+                                    "y": 1
+                                }]
+                            }, {
+                                "x": 1,
+                                "y": 1,
+                                "cp": [{
+                                    "x": 0.3,
+                                    "y": 1
+                                }]
+                            }],
+                            delay: (itemstotal - pos - 1) * 40
+                        };
+                    }
+                }
+            });
+        })();
 });
